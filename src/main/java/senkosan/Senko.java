05c70cb30e8a7af451b16a7a1669144bb885fdf5
JJ Universe <46922562+JJ-Universe@users.noreply.github.com>
2022-03-05 13:33:45 -0800

    Removed Bot Token

--- src/main/java/senkosan/Senko.java
+++ src/main/java/senkosan/Senko.java
@@ -9,7 +9,7 @@ import net.dv8tion.jda.api.entities.Activity;
 public class Senko {
 	
 	public static void main(String[] args) throws LoginException {
-		JDABuilder jda = JDABuilder.createDefault("OTQ4MDYzNTQzODEzNjMyMDEw.Yh2W-w.VubWkXUnHyaG1Bnkym3wN60x5YY");
+		JDABuilder jda = JDABuilder.createDefault("");
 		jda.setActivity(Activity.watching("Season 2"));
 		jda.setStatus(OnlineStatus.ONLINE);
 		jda.addEventListeners(new Listener());
